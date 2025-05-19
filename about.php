5d8742cc363eb1278a34a3fdb034aa23cb19d487
SG-HtetAungLinn <softguide.htetaunglinn@gmail.com>
2025-05-19 23:29:10 +0630

    first commit

--- about.php
+++ about.php
@@ -5,7 +5,10 @@ require "./layouts/header.php";
 ?>
 <div class="container py-5">
     <section>
-        <h1 class="text-center">About Us</h1>
+        <div class="text-center mb-5">
+            <h1 class="fw-bold text-theme">About Us</h1>
+            <div class="border-bottom border-3 border-primary w-25 mx-auto mt-3"></div>
+        </div>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit rerum optio quis eius laudantium soluta quibusdam? Aliquid sequi cum quos, iste hic expedita obcaecati delectus illum consectetur possimus quis dolores.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit rerum optio quis eius laudantium soluta quibusdam? Aliquid sequi cum quos, iste hic expedita obcaecati delectus illum consectetur possimus quis dolores.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit rerum optio quis eius laudantium soluta quibusdam? Aliquid sequi cum quos, iste hic expedita obcaecati delectus illum consectetur possimus quis dolores.</p>
